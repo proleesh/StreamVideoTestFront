e3495c53594fd32192899f6dada3e7ab2f929dbf
proleesh <merci726@yahoo.com>
2024-09-07 21:03:55 +0900

    update about test video

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Toaster } from "react-hot-toast";
 import "./App.css";
 import VideoUpload from "./assets/components/VideoUpload";
+import { useState } from "react";
 
 function App() {
+  const [videoId, setVideoId] = useState(
+    "cf890bf6-cfd5-4c90-832d-1e46592f5dad"
+  );
   return (
     <>
       <Toaster />
@@ -10,7 +14,20 @@ function App() {
         <h1 className="text-5xl font-bold text-gray-700 dark:text-gray-100">
           Proleesh SVS
         </h1>
-        <VideoUpload />
+
+        <div className="flex mt-14 w-full justify-around">
+          <div>
+            <h1 className="text-black">Playing Video</h1>
+            <video
+              style={{
+                width: 500,
+              }}
+              src={`http://localhost:8080/api/v1/videos/stream/${videoId}`}
+              controls
+            ></video>
+          </div>
+          <VideoUpload />
+        </div>
       </div>
     </>
   );
